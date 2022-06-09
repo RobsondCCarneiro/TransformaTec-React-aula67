afb8acda28830ed0b66da2ce4b7a1d2f7a9c9f8f
RobsondCCarneiro <robinho.cenep@gmail.com>
2022-06-09 11:39:47 -0300

    Semaforo no React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Todos from './TodoList/Todos'
+import TrafficLight from './TrafficLights/TrafficLight';
+import './TrafficLights/styles.css'
 
 function App() {
   return (
     <div className = 'App'>
-      <Todos />
+      {/* <Todos /> */}
+      <TrafficLight initialValue={0}/>
+      <TrafficLight initialValue={1}/>
     </div>
   );
 }
